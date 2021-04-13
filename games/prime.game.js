7aab8f9065bfb2e3a24e0ec56f6d16ecb380d74b
Vyacheslav Maletin <Maletin.V95@gmail.com>
2021-04-13 23:07:26 +0500

    corrected file prime.game.js

--- games/prime.game.js
+++ games/prime.game.js
@@ -9,7 +9,12 @@ import {
 // eslint-disable-next-line import/prefer-default-export
 export const primeGame = () => {
   const userName = greetingAndRememberName('Answer "yes" if given number is prime. Otherwise answer "no".');
-  const isPrime = (num) => (num % 2 === 0);
+  const isPrime = (num) => {
+    for (let i = 2; i < num; i += 1) {
+      if (num % i === 0) return false;
+    }
+    return num > 1;
+  };
   const check = (num) => {
     const result = isPrime(num) ? 'yes' : 'no';
     return result;
